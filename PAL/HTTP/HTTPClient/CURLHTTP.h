28a084cce10abfaefe51125e86a73a1a449e7f9c
Ilya Klementev <klema2727@gmail.com>
2021-07-30 09:51:16 +0300

    Linux curl update

--- PAL/HTTP/HTTPClient/CURLHTTP.h
+++ PAL/HTTP/HTTPClient/CURLHTTP.h
@@ -15,17 +15,20 @@ size_t writeFunction(void *ptr,
 namespace PAL {
 	class CURLHTTP : public IHTTPClient {
 	public:
-		void sendRequest(Request& rq, Response& rs) {
+		std::string sendRequest(Request& rq, Response& rs) {
 			std::string foo = rq.getUrl();
 
 			auto curl = curl_easy_init();
 			if (curl) {
 				curl_easy_setopt(curl, CURLOPT_URL, foo.c_str());
-				std::string response_string;
-				std::string header_string;
+				curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+				errbuf[0] = 0;
+				std::string response_status;
+				std::string response_header;
+				std::string response_data;
 				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
-				curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
-				curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
+				curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
+				curl_easy_setopt(curl, CURLOPT_HEADERDATA, &response_header);
 
 				char* url;
 				long response_code;
@@ -34,13 +37,28 @@ namespace PAL {
 				curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
 				curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
 
-				curl_easy_perform(curl);
+				//curl_easy_perform(curl);
+				res = curl_easy_perform(curl);
 				curl_easy_cleanup(curl);
 				curl = nullptr;
-				rs.setData(response_string);
-				rs.setHeader(header_string);
+
+				if(res != CURLE_OK){
+					m_status = errbuf;
+				}
+
+				response_status.assign(
+					response_header, 0, response_header.find('\n'));
+				response_header.erase(0, response_header.find('\n'));
+				rs.setStatus(response_status);
+				rs.setHeader(response_header);
+				rs.setData(response_data);
 			}
+			return m_status;
 		}
+	private:
+		std::string m_status { "Successful sending of data" };
+		CURLcode res;
+		char errbuf[CURL_ERROR_SIZE];
 	};
 }
 
